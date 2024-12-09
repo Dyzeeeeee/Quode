d4832ff9b24dcfb831bfa87a6e114e5ca9438872
patrick333c <jdmalaluan2@gmail.com>
2024-12-09 13:18:37 +0800

    Update Initial.vue

--- src/views/Initial.vue
+++ src/views/Initial.vue
@@ -199,7 +199,7 @@ watch(password, (newPassword) => {
 
     if (pwModal.value && newPassword === '0000') {
         pwModal.value = false; // Close the modal
-        router.push({ name: 'user', params: { id: selectedSectionId } }); // Redirect
+        router.push({ name: 'user', params: { id: selectedSectionId } });
     }
 });
 // Constants
